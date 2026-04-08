58121ce338388026474143c4d90b10a77e53714d
praneel46 <praneelkulkarni4646@gmail.com>
2026-04-08 15:05:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -435,7 +435,8 @@ def delete_student(student_id):
     flash("Student deleted successfully!", "success")
     return redirect(url_for("students"))
 
+import os
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
